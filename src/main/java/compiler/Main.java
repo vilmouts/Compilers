0d47b16cf0ecb22032bfd29833bf880fc7ac666e
vilmouts <vilmouts@hotmail.com>
2017-03-19 23:51:58 +0200

    First commit

---
+++ src/main/java/compiler/Main.java
@@ -0,0 +1,41 @@
+/*
+package compiler;
+
+class Main { 
+
+	public static void main(String args[]) { 
+		System.out.println("Hello World"); // prints Hello World
+	}
+
+}
+*/
+package compiler;
+
+import compiler.lexer.Lexer;
+import compiler.node.*;
+import java.io.PushbackReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+        public static void main(String args[]) {
+                PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));   
+                Lexer lexer = new Lexer(reader);
+
+                for(;;) {
+                        try {
+                                Token t = lexer.next();
+
+                        if (t instanceof EOF)
+                                break;
+                        System.out.println(t.toString());
+                        } catch (Exception e)
+                        {
+                                System.err.println(e.getMessage());
+                        }
+                }
+
+                System.exit(0);
+        }
+
+}
